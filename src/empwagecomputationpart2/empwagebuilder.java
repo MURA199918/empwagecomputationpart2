36f90eff792eb2fcd4a65e2c8cb98c4688bbcc7e
MURA199918 <muralisaranr18@gmail.com>
2020-11-19 18:22:40 +0530

    added UC8

--- src/empwagecomputationpart2/empwagebuilder.java
+++ src/empwagecomputationpart2/empwagebuilder.java
@@ -3,35 +3,36 @@ package empwagecomputationpart2;
 public class empwagebuilder {
 	 public static final int IS_PART_TIME = 1;
 	    public static final int IS_FULL_TIME = 2;
-	    public static final int EMP_RATE_PER_HOUR = 20;
-	    public static final int NUM_OF_WORKING_DAYS = 2;
-	    public static final int MAX_HRS_IN_MONTH = 10;
-	    public static int computeempwage() {
-	        int emphrs = 0;
-	        int totalemphrs = 0;
-	        int totalworkingdays = 0;
-	        while(totalemphrs<=MAX_HRS_IN_MONTH && totalworkingdays<NUM_OF_WORKING_DAYS) {
-	            totalworkingdays++;
-	            int empcheck = (int) Math.floor(Math.random()*10)%3;
-	            switch(empcheck) {
-	                case IS_PART_TIME:
-	                    emphrs = 4;
-	                    break;
-	                case IS_FULL_TIME:
-	                    emphrs = 8;
-	                    break;
-	                default:
-	                    emphrs = 0;
-	            }
-	            totalemphrs+=emphrs;
-	            System.out.println("Day:"+totalworkingdays+"emp hr:"+emphrs);
-	        }
-	        int totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
-	        System.out.println("Total emp wage:" + totalempwage);
-	        return totalempwage;
+	   
+	    public static int computeempwage(String company, int emprateperhour,int numofworkingdays,int maxhourspermonth){
+	    	 int emphrs = 0;
+		        int totalemphrs = 0;
+		        int totalworkingdays = 0;
+		        while(totalemphrs<=maxhourspermonth && totalworkingdays<numofworkingdays) {
+		            totalworkingdays++;
+		            int empcheck = (int) Math.floor(Math.random()*10)%3;
+		            switch(empcheck) {
+		                case IS_PART_TIME:
+		                    emphrs = 4;
+		                    break;
+		                case IS_FULL_TIME:
+		                    emphrs = 8;
+		                    break;
+		                default:
+		                    emphrs = 0;
+		            }
+		            totalemphrs+=emphrs;
+		            System.out.println("Day:"+totalworkingdays+"emp hr:"+emphrs);
 	    }
+		        int totalempwage = totalemphrs * emprateperhour;
+		        System.out.println("Total emp wage:" + totalempwage);
+		        return totalempwage;   
+	       
+	        }
+	        
 	    public static void main(String[] args) {
-	        computeempwage();
+	        computeempwage("dmart",20,2,10);
+	        computeempwage("reliance",10,4,20);
 	    }
 
 }
