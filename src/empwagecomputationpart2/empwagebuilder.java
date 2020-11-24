60e4b4fcf4104eeccc103e37ac5745e3c862418d
MURA199918 <muralisaranr18@gmail.com>
2020-11-24 08:20:55 +0530

    added UC12

--- src/empwagecomputationpart2/empwagebuilder.java
+++ src/empwagecomputationpart2/empwagebuilder.java
@@ -4,26 +4,32 @@ import java.util.*;
 public class empwagebuilder {
 	public static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Enter number of companies");
-		int noofcompany=sc.nextInt();
-		companyempwage[] companies=new companyempwage[noofcompany];
-		for(int i=0;i<companies.length;i++) {
-			System.out.println("Name of company");
-			String name=sc.next();
-			System.out.println("Enter working days");
-			int workingdays=sc.nextInt();
-			System.out.println("Enter wage per hour");
-			int wageperhour=sc.nextInt();
-			System.out.println("Working hours in a day");
-			int workinghrsinday=sc.nextInt();
-			companyempwage c=new companyempwage(name,workingdays,wageperhour,workinghrsinday);
-			double monthlywage=c.calculateempwage();
-			System.out.println(name+" monthly wage"+monthlywage);
-			companies[i]=c;
+		ArrayList<companyempwage> companies=new ArrayList<>();
+		int select=1;
+		while(select==1) {
+			System.out.println("1.Add 2.Exit");
+			int option=sc.nextInt();
+			switch(option) {
+			case 1:
+				System.out.println("Name of company");
+				String name=sc.next();
+				System.out.println("Enter working days");
+				int workingdays=sc.nextInt();
+				System.out.println("Enter wage per hour");
+				int wageperhour=sc.nextInt();
+				System.out.println("Working hours in a day");
+				int workinghrsinday=sc.nextInt();
+				companyempwage c=new companyempwage(name,workingdays,wageperhour,workinghrsinday);
+				double monthlywage=c.calculateempwage();
+				System.out.println(name+" monthly wage"+monthlywage);
+				companies.add(c);
+				select=1;
+				break;
+			case 2:
+				System.out.println("Thankyou");
+				select=0;
+				break;
+			}
 		}
-    }
-	
-}
-public interface computewageinterface{
-	public double calculateempwage();
-}
+	}
+}	
